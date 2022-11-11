b51fbe7c2c1b5d7921ce35ffff16622cb1538e96
Brandon Fain <brandon.fain@duke.edu>
2022-11-11 11:23:48 -0500

    Initial commit

---
+++ src/GraphDemo.java
@@ -0,0 +1,5 @@
+public class GraphDemo {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/GraphProcessor.java
@@ -0,0 +1,76 @@
+import java.security.InvalidAlgorithmParameterException;
+import java.util.List;
+
+public class GraphProcessor {
+    /**
+     * Creates and initializes a graph from a source data
+     * file in the .graph format. Should be called
+     * before any other methods work.
+     * @param fileName name of source .graph file
+     * @throws Exception if file not found or error reading
+     */
+    public void initialize(String fileName) throws Exception {
+        // TODO: Implement initialize
+    }
+
+
+    /**
+     * Searches for the point in the graph that is closest in
+     * straight-line distance to the parameter point p
+     * @param p A point, not necessarily in the graph
+     * @return The closest point in the graph to p
+     */
+    public Point nearestPoint(Point p) {
+        // TODO: Implement initialize
+        return null;
+    }
+
+
+    /**
+     * Calculates the total distance along the route, summing
+     * the distance between the first and the second Points, 
+     * the second and the third, ..., the second to last and
+     * the last. Distance returned in miles.
+     * @param start Beginning point. May or may not be in the graph.
+     * @param end Destination point May or may not be in the graph.
+     * @return The distance to get from start to end
+     */
+    public double routeDistance(List<Point> route) {
+        // TODO Implement routeDistance
+        return 0.0;
+    }
+    
+
+    /**
+     * Checks if input points are part of a connected component
+     * in the graph, that is, can one get from one to the other
+     * only traversing edges in the graph
+     * @param p1 one point
+     * @param p2 another point
+     * @return true if p2 is reachable from p1 (and vice versa)
+     */
+    public boolean connected(Point p1, Point p2) {
+        // TODO: Implement connected
+        return false;
+    }
+
+
+    /**
+     * Returns the shortest path, traversing the graph, that begins at start
+     * and terminates at end, including start and end as the first and last
+     * points in the returned list. If there is no such route, either because
+     * start is not connected to end or because start equals end, throws an
+     * exception.
+     * @param start Beginning point.
+     * @param end Destination point.
+     * @return The shortest path [start, ..., end].
+     * @throws InvalidAlgorithmParameterException if there is no such route, 
+     * either because start is not connected to end or because start equals end.
+     */
+    public List<Point> route(Point start, Point end) throws InvalidAlgorithmParameterException {
+        // TODO: Implement route
+        return null;
+    }
+
+    
+}
